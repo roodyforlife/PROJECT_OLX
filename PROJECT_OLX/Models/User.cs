a2e55a13c2298073fc2491778b2ff1ea0eb67366
Matvey <kolossmatveyka@gmail.com>
2022-03-28 19:15:36 +0300

    val

--- PROJECT_OLX/Models/User.cs
+++ PROJECT_OLX/Models/User.cs
@@ -11,10 +11,12 @@ namespace PROJECT_OLX.Models
     public class User
     {
         [Key]
+        [RegularExpression(@"[A-Za-z0-9_]+", ErrorMessage = "У імені може бути лише латинські літери та знак '_'")]
         [IsSpace(ErrorMessage = "Ім'я не повинно містити пробілів")]
         [Required(ErrorMessage = "Це обов'язкове поле")]
         [StringLength(12, MinimumLength = 3, ErrorMessage = "Довжина імені має бути від 3 до 12 символів")]
         public string Name { get; set; }
+        [RegularExpression(@"[A-Za-z0-9]+", ErrorMessage = "У паролі може бути лише латинські літери")]
         [IsSpace(ErrorMessage = "Пароль не повиннен містити пробілів")]
         [Required(ErrorMessage = "Це обов'язкове поле")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "Довжина пароля має бути від 6 до 20 символів")]
